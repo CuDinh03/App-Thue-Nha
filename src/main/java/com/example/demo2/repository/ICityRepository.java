25a2e080610640e01a920f8f1d3ba8f0efe2517f
CuDinh03 <dinhhuycu0305@gmail.com>
2024-01-07 23:53:50 +0700

    update code

--- src/main/java/com/example/demo2/repository/ICityRepository.java
+++ src/main/java/com/example/demo2/repository/ICityRepository.java
@@ -2,8 +2,10 @@ package com.example.demo2.repository;
 
 import com.example.demo2.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+@Repository
 
 public interface ICityRepository extends JpaRepository<City, UUID> {
 }
--- src/main/java/com/example/demo2/repository/IRenterRepository.java
+++ src/main/java/com/example/demo2/repository/IRenterRepository.java
@@ -2,8 +2,10 @@ package com.example.demo2.repository;
 
 import com.example.demo2.model.Renter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+@Repository
 
 public interface IRenterRepository extends JpaRepository<Renter, UUID> {
 }
